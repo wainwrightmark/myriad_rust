81d995e95a216294981fd8ac1c15cde185174517
Mark Wainwright <wainwrightml@gmail.com>
2022-05-07 11:18:56 +0100

    tabs now move with most recent goal

--- src/state/foundwordsstate.rs
+++ src/state/foundwordsstate.rs
@@ -5,26 +5,31 @@ use std::collections::BTreeMap;
 
 #[derive(PartialEq, Clone, Serialize, Deserialize, Default)]
 pub struct FoundWordsState {
+    pub most_recent: Option<i32>,
     pub words: BTreeMap<i32, FoundWord>,
 }
-
 
 impl FoundWordsState {
     pub fn with_word(&self, word: FoundWord) -> Self {
         let mut new_map = self.words.clone();
 
-        new_map.insert(word.result, word);
+        let i = word.result;
+        new_map.insert(i, word);
 
-        FoundWordsState { words: new_map }
+        FoundWordsState {
+            words: new_map,
+            most_recent: Some(i),
+        }
     }
 
     pub fn has_word(&self, word: &FoundWord) -> bool {
         self.words.contains_key(&word.result)
     }
 
-    pub fn has_all_words(&self, range: &mut Range<i32>)->bool
-    {
-        if self.words.len() < range.size_hint().0 {return false;}           
+    pub fn has_all_words(&self, range: &mut Range<i32>) -> bool {
+        if self.words.len() < range.size_hint().0 {
+            return false;
+        }
 
         range.all(|x| self.words.contains_key(&x))
     }
