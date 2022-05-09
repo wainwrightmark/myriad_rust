2b727de16ea7ef8041434b901c7f578f486f74ed
Mark Wainwright <wainwrightml@gmail.com>
2022-05-09 22:39:02 +0100

    more

--- src/web/mod.rs
+++ src/web/mod.rs
@@ -1,8 +1,16 @@
 automod::dir!("src/web");
 
 pub const SQUARE_SIZE: f64 = 120.0;
-pub const SVG_WIDTH: f64 = 360.0;
-pub const SVG_HEIGHT: f64 = 360.0;
+pub const SVG_WIDTH: f64 = SQUARE_SIZE * 3.0;
+pub const SVG_HEIGHT: f64 = SQUARE_SIZE * 2.0 + TAB_HEADER_HEIGHT + FOUND_WORD_HEIGHT * 2.0 + FOUND_WORD_MARGIN * 3.0;
+
+pub const TAB_HEADER_WIDTH : f64 = 60.0;
+pub const TAB_HEADER_HEIGHT : f64 = 60.0;
+
+pub const FOUND_WORD_WIDTH : f64 = 30.0;
+pub const FOUND_WORD_HEIGHT : f64 = 30.0;
+pub const FOUND_WORD_MARGIN : f64 = 5.0;
+pub const FOUND_WORD_PADDING : f64 = SVG_WIDTH - (FOUND_WORD_WIDTH * 10.0 + FOUND_WORD_MARGIN * 9.0);
 
 
 pub mod prelude {
