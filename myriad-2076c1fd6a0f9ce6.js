96422f31c2e576ae67ed0701fc40a1c3b086329a
wainwrightmark <wainwrightmark@users.noreply.github.com>
2022-05-12 20:56:38 +0000

    deploy: e6d7098a23a9bab7f00e5a9bcd33d6375fcc1023

--- myriad-2076c1fd6a0f9ce6.js
+++ myriad-2076c1fd6a0f9ce6.js
@@ -285,7 +285,7 @@ async function load(module, imports) {
 
 async function init(input) {
     if (typeof input === 'undefined') {
-        input = new URL('myriad-926bea1bfe11ad58_bg.wasm', import.meta.url);
+        input = new URL('myriad-2076c1fd6a0f9ce6_bg.wasm', import.meta.url);
     }
     const imports = {};
     imports.wbg = {};
@@ -654,11 +654,11 @@ async function init(input) {
         const ret = getObject(arg0).sessionStorage;
         return isLikeNone(ret) ? 0 : addHeapObject(ret);
     }, arguments) };
-    imports.wbg.__wbindgen_closure_wrapper1211 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1210 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 48, __wbg_adapter_22);
         return addHeapObject(ret);
     };
-    imports.wbg.__wbindgen_closure_wrapper1945 = function(arg0, arg1, arg2) {
+    imports.wbg.__wbindgen_closure_wrapper1944 = function(arg0, arg1, arg2) {
         const ret = makeMutClosure(arg0, arg1, 48, __wbg_adapter_25);
         return addHeapObject(ret);
     };
