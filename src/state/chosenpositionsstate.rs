2f1f870aadc32e58c733290743fe897854170975
Mark Wainwright <wainwrightml@gmail.com>
2022-05-20 10:41:27 +0100

    Moved ChosenPositions into their own state object

--- src/state/chosenpositionsstate.rs
+++ src/state/chosenpositionsstate.rs
@@ -1,7 +1,10 @@
 use crate::core::prelude::*;
+use crate::state::prelude::*;
+use itertools::Itertools;
 use serde::*;
+use yewdux::prelude::*;
 
-#[derive(PartialEq, Clone, Default, Serialize, Deserialize)]
+#[derive(PartialEq, Clone, Default, Serialize, Deserialize, Store)]
 pub struct ChosenPositionsState {
     pub positions: Vec<Coordinate>,
 }
@@ -33,5 +36,76 @@ impl ChosenPositionsState {
             },
             MoveResult::IllegalMove => self,
         }
+    }
+}
+
+pub struct OnClickMsg {
+    pub coordinate: Coordinate,
+}
+
+pub struct FindNumberMsg {
+    pub number: i32,
+}
+
+impl Reducer<ChosenPositionsState> for FindNumberMsg {
+    fn apply(&self, state: std::rc::Rc<ChosenPositionsState>) -> std::rc::Rc<ChosenPositionsState> {
+        let fs = Dispatch::<FullState>::new().get();
+
+        if let Some(path) = fs.found_words.words.get(&self.number) {
+            ChosenPositionsState {
+                positions: path.path.clone(),
+            }
+            .into()
+        } else {
+            state
+        }
+    }
+}
+
+impl Reducer<ChosenPositionsState> for OnClickMsg {
+    fn apply(&self, state: std::rc::Rc<ChosenPositionsState>) -> std::rc::Rc<ChosenPositionsState> {
+        let coordinate = self.coordinate;
+
+        if let Some(last) = state.positions.last() {
+            if last == &coordinate {
+                //Abandon word - empty state
+                Dispatch::new().apply(ClearExpiredWordsMsg {});
+
+                return ChosenPositionsState::default().into();
+            }
+        }
+
+        let find_result = state.positions.iter().find_position(|&z| z == &coordinate);
+
+        if let Some((index, _)) = find_result {
+            let new_chosen_positions: Vec<Coordinate> = state
+                .positions
+                .iter()
+                .take(index + 1)
+                .copied()
+                .collect_vec();
+
+            //TOOD maybe send a find message here
+
+            return ChosenPositionsState {
+                positions: new_chosen_positions,
+            }
+            .into(); //Retrace move
+        }
+
+        if state.positions.is_empty() || state.positions.last().unwrap().is_adjacent(&coordinate) {
+            let mut new_chosen_positions = state.positions.clone();
+            new_chosen_positions.push(coordinate);
+
+            Dispatch::new().apply(OnCoordinatesSetMsg {
+                coordinates: new_chosen_positions.clone(),
+            });
+
+            return ChosenPositionsState {
+                positions: new_chosen_positions,
+            }
+            .into(); //New move
+        }
+        return state;
     }
 }
