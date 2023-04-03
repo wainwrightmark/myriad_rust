85ac0ec7a103a212c45f9fb0e2a20b5af6bde911
Mark Wainwright <wainwrightml@gmail.com>
2023-04-03 13:28:00 +0100

    enabled cheat

--- myriad_yew/src/web/app.rs
+++ myriad_yew/src/web/app.rs
@@ -1,45 +1,19 @@
 use crate::state::prelude::*;
 use crate::web::found_words::*;
 use crate::web::prelude::*;
-use serde::Deserialize;
-use serde::Serialize;
 use yew::prelude::*;
+use yew_hooks::use_search_param;
 use yewdux::prelude::Dispatch;
-// #[derive(Clone, Routable, PartialEq)]
-// enum Route {
-//     #[at("/Cheat")]
-//     Cheat,
-//     #[at("/")]
-//     Home,
-// }
-
-#[derive(Clone, PartialEq, Eq, Serialize, Deserialize, Default)]
-pub struct RouteQuery {
-    #[serde(default)]
-    pub cheat: bool,
-}
-
-// #[function_component(App)]
-// pub fn app() -> Html {
-//     html! {
-//         <BrowserRouter>
-//             <Switch<Route> render={myriad_app} />
-//         </BrowserRouter>
-//     }
-// }
 
 #[function_component(App)]
-pub fn myriad_app(//route : Route
-) -> Html {
+pub fn myriad_app() -> Html {
+    let cheat = use_search_param("cheat".to_string()).map(|x|x.to_ascii_lowercase() =="true").unwrap_or_default();
     let node = use_node_ref();
-
     let (width, height) = yew_hooks::use_size(node.clone());
 
     Dispatch::<GameSize>::new().apply(SetSizeMessage { width, height });
 
-    let cheat = false;
-
-    if width == 0 && height == 0{
+    if width == 0 && height == 0 {
         return html!(
             <div class="container" ref={node}/>
         );
