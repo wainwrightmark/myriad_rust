42be17ecee83a09115cc197d2cb7ff2d6a2f0c41
Mark Wainwright <wainwrightml@gmail.com>
2023-04-17 15:24:46 +0100

    lambda function now generates basic image

---
+++ netlify/functions/image/src/main.rs
@@ -0,0 +1,77 @@
+use aws_lambda_events::encodings::Body;
+use aws_lambda_events::event::apigw::{ApiGatewayProxyRequest, ApiGatewayProxyResponse};
+use http::header::HeaderMap;
+use http::HeaderValue;
+use lambda_runtime::{handler_fn, Context, Error};
+use log::LevelFilter;
+use simple_logger::SimpleLogger;
+use tiny_skia::*;
+
+#[tokio::main]
+async fn main() -> Result<(), Error> {
+    SimpleLogger::new()
+        .with_level(LevelFilter::Info)
+        .init()
+        .unwrap();
+
+    let func = handler_fn(my_handler);
+    lambda_runtime::run(func).await?;
+    Ok(())
+}
+
+pub(crate) async fn my_handler(
+    _event: ApiGatewayProxyRequest,
+    _ctx: Context,
+) -> Result<ApiGatewayProxyResponse, Error> {
+    //let path = event.path.unwrap();
+    //_event.
+
+    let mut headers = HeaderMap::new();
+    headers.insert("Content-Type", HeaderValue::from_static("image/png"));
+
+    let data = draw_image();
+
+    let resp = ApiGatewayProxyResponse {
+        status_code: 200,
+        headers,
+        multi_value_headers: HeaderMap::new(),
+        body: Some(Body::Binary(data)),
+        is_base64_encoded: Some(false),
+    };
+
+    Ok(resp)
+}
+
+fn draw_image() -> Vec<u8> {
+    let mut paint = Paint::default();
+    paint.shader = LinearGradient::new(
+        Point::from_xy(100.0, 100.0),
+        Point::from_xy(900.0, 900.0),
+        vec![
+            GradientStop::new(0.0, Color::from_rgba8(50, 127, 150, 200)),
+            GradientStop::new(1.0, Color::from_rgba8(220, 140, 75, 180)),
+        ],
+        SpreadMode::Pad,
+        Transform::identity(),
+    )
+    .unwrap();
+
+    let mut pb = PathBuilder::new();
+    pb.move_to(60.0, 60.0);
+    pb.line_to(160.0, 940.0);
+    pb.cubic_to(380.0, 840.0, 660.0, 800.0, 940.0, 800.0);
+    pb.cubic_to(740.0, 460.0, 440.0, 160.0, 60.0, 60.0);
+    pb.close();
+    let path = pb.finish().unwrap();
+
+    let mut pixmap = Pixmap::new(1000, 1000).unwrap();
+    pixmap.fill_path(
+        &path,
+        &paint,
+        FillRule::Winding,
+        Transform::identity(),
+        None,
+    );
+
+    pixmap.encode_png().unwrap()
+}
