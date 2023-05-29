28e8b07ddd4fdbfa30175ce6220ece31cc56ea3a
Mark Wainwright <wainwrightml@gmail.com>
2023-05-29 07:51:10 +0100

    updated edge functions

--- netlify/edge-functions/og-param-proxy.js
+++ netlify/edge-functions/og-param-proxy.js
@@ -9,24 +9,24 @@ export default async (request, context) => {
     const game = url.pathname.substring(6);
 
     page = page.replace(
-      "https://myriad-game.com/icon/og_image_square.png",
+      `https://myriad-game.com/icon/og_image_square.png`,
       `https://myriad-game.com/.netlify/functions/image?level=${game}&width=1080&height=1080`
     );
 
     page = page.replace(
-      "https://myriad-game.com/icon/og_image_landscape.png",
+      `https://myriad-game.com/icon/og_image_landscape.png`,
       `https://myriad-game.com/.netlify/functions/image?level=${game}&width=1200&height=630`
     );
 
     page = page.replace(
-      '<meta property="og:url" content="https://myriad-game.com" />',
-      `<meta property="og:url" content="https://myriad-game.com/game/${game}" />`
+      `<meta property="og:url" content="https://myriad-game.com"`,
+      `<meta property="og:url" content="https://myriad-game.com/game/${game}"`
     );
 
 
-    page = page.replaceAll(
-        '<meta property="og:image:alt" content="The Myriad Logo" />',
-        `<meta property="og:image:alt" content="The Myriad Game: ${game}" />`
+    page = page.replace(
+        `<meta property="og:image:alt" content="The Myriad Logo"`,
+        `<meta property="og:image:alt" content="The Myriad Game: ${game}"`
     )
     return new Response(page, response);
   } catch {
